e12effd029023e91df4ba40ef46e9ef42f7b1428
bunxbio <biokenfigumilang@gmail.com>
2021-08-30 21:58:48 +0700

    adding config file and seeder

---
+++ seeder/seeder.rb
@@ -0,0 +1,33 @@
+disable_script = "await sleep(2000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);  
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate('window.scrollBy(0,1200)'); await sleep(1000);
+await page.evaluate(() => {
+    for (const script of document.querySelectorAll('script[data-webfe]')) {
+        script.parentNode.removeChild(script);
+    }
+  });
+await sleep(1000);
+"
+
+pages << {
+  url: 'https://shopee.co.id/Laptop-col.832738?page=0',
+  page_type: 'listings',
+  fetch_type: 'browser',
+  method: 'GET',
+  force_fetch: true,
+  headers: {
+    "User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+  },
+  driver: {
+    enable_images: false,
+    code: disable_script
+  }
+}
